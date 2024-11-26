8cc76e65dfc16d06fb99735e0ea38ea614f2e24d
tdash_31 <dasha.tikhonova.2002@mail.ru>
2024-11-27 01:48:29 +0300

    Удалена папка tauri из ветки page

--- tauri/backend/app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import *
-
-admin.site.register(Specialist)
-admin.site.register(Lecture)
-admin.site.register(SpecialistLecture)
--- tauri/frontend/src/modules/types.ts
+++
@@ -1,7 +0,0 @@
-export type T_Specialist =  {
-    id: number,
-    name: string,
-    description: string,
-    image?: string,
-    status: number
-}
